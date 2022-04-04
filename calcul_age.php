12c0c8a18b1c3e4e2869044b4773b7cdcb7ba5d1
LoicPfeiffer <95574385+LoicPfeiffer@users.noreply.github.com>
2022-04-04 22:54:27 +0200

    Add files via upload

---
+++ calcul_age.php
@@ -0,0 +1,20 @@
+﻿<?php  
+    $action = filter_var($_SERVER['PHP_SELF'], FILTER_VALIDATE_URL);
+
+    $saisie = $_GET['saisie'] ?? '-1';
+    $age = intval($saisie);
+    $annee = date('Y');
+    $ageValide = $age >= 1 && $age <= 130;
+    echo('$ageValide='); var_dump($ageValide); echo '<br>';
+?>
+    <form method="GET" action="<?= $action ?>">
+        <label for="saisie">Votre âge au 31/12/<?= $annee ?> </label>
+        <input type="text" name="saisie" value="<?= $saisie ?? '' ?>"/>
+        <input type="submit" value="envoyer">
+    </form>
+
+
+<?php
+    $naiss = $annee - $age;
+    echo 'Vous êtes né en ' . $naiss;
+?>
